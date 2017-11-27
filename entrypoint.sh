27b10d0f091fe524079c09225f755632ab3bac99
moeller <moeller@mecom.de>
2017-11-27 14:07:00 +0100

    add index-script after import

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 for file in `ls /data/*.osm.pbf`; do
-  osm2pgsql -d $PG_DATABASE --create --slim  -G --hstore --tag-transform-script ~/src/openstreetmap-carto/openstreetmap-carto.lua \
-    -C 2500 --number-processes 1 -S ~/src/openstreetmap-carto/openstreetmap-carto.style /data/$file \
-    -H $PG_HOST -P $PG_PORT -U $PG_USER  -W $PG_PASSWORD
+  osm2pgsql -d $PGDATABASE --create --slim  -G --hstore --tag-transform-script /src/openstreetmap-carto/openstreetmap-carto.lua \
+    -C 2500 --number-processes 1 -S /src/openstreetmap-carto/openstreetmap-carto.style /data/$file \
+    -H $PGHOST -P $PGPORT -U $PGUSER  -W $PGPASSWORD
 
 done
+
+/src/openstreetmap-carto/scripts/indexes.py | psql -d $PGDATABASE -h $PGHOST -P $PGPORT -U $PGUSER  -W $PGPASSWORD
